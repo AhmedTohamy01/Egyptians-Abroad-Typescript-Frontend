ba25470df32a591a23ed31d54451432ed85d39da
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2021-08-28 18:03:00 +0200

    feat: add MainContext provider in _app.tsx file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import type { AppProps } from 'next/app'
 import GlobalStyles from '../styles/GlobalStyles'
+import { MainContextProvider } from '../context/MainContext'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <MainContextProvider>
       <GlobalStyles />
       <Component {...pageProps} />
-    </>
+    </MainContextProvider>
   )
 }
 export default MyApp
